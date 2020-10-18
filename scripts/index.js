6f9a0e56b5cff95035ccaee13cd3639c2223c1aa
Aleksandra Perepechenko <cassandra@yandex.ru>
2020-10-18 21:15:45 +0300

    hotfix: исправление ошибок проверки 1

--- scripts/index.js
+++ scripts/index.js
@@ -27,14 +27,11 @@ const initialCards = [
 
 const placeContainer = document.querySelector('.elements');
 const addPlace = document.querySelector('.lead__add');
-let placeInput= document.querySelector('.popup__input_type_place');
-let urlInput = document.querySelector('.popup__input_type_image');
+const placeInput= document.querySelector('.popup__input_type_place');
+const urlInput = document.querySelector('.popup__input_type_image');
 const cardTemplate = document.querySelector('#element-template').content;
-
 const popupProfile = document.querySelector('.popup_type_profile');
-const editProfileButton = document.querySelector('.lead__edit');
 const popupPlace = document.querySelector('.popup_type_place');
-const addPlaceButton = document.querySelector('.lead__add');
 const closeButtomPlace = popupPlace.querySelector('.popup__close');
 const formElementPlace = popupPlace.querySelector('.popup__container');
 const editButtom = document.querySelector('.lead__edit');
@@ -45,8 +42,8 @@ const profileName = profile.querySelector('.lead__name');
 const profileProffesion = profile.querySelector('.lead__proffesion');
 const nameInput = popupProfile.querySelector('.popup__input_type_name');
 const jobInput = popupProfile.querySelector('.popup__input_type_profession');
-const FullSizePhoto = document.querySelector('.popup_img');
-const FullSizePhotoTitle = document.querySelector('.popup_img-title');
+const fullsizePhoto = document.querySelector('.popup_img');
+const fullsizePhotoTitle = document.querySelector('.popup_img-title');
 const popupPhoto = document.querySelector('.popup_type_photo');
 const closeButtomPhoto = popupPhoto.querySelector('.popup__close');
 
@@ -75,14 +72,13 @@ const getCard = (data) => {
   const likeButton = card.querySelector('.element__heart');
   const deleteButton = card.querySelector('.element__delete');
   const openFullSizePhotoButton = card.querySelector('.element__photo');
-  const PhotoTitle = card.querySelector('.element__title');
+  const photoTitle = card.querySelector('.element__title');
   
   likeButton.addEventListener('click', likeCard);
   deleteButton.addEventListener('click', deleteCard);
   openFullSizePhotoButton.addEventListener('click', function() {
-    console.log(FullSizePhoto)
-    FullSizePhoto.src = openFullSizePhotoButton.src;
-    FullSizePhotoTitle.textContent = PhotoTitle.textContent;
+    fullsizePhoto.src = openFullSizePhotoButton.src;
+    fullsizePhotoTitle.textContent = fullsizePhoto.textContent;
     togglePopup(popupPhoto);
   });
   return card;
@@ -98,17 +94,17 @@ function togglePopup(item) {
 
 const bindHandlers = () => {
   formElementPlace.addEventListener('submit', (evt) => {
-  evt.preventDefault(); 
-  const newCard = getCard({
-        name: placeInput.value,
-        link: urlInput.value
-      });
+    evt.preventDefault(); 
+    const newCard = getCard({
+      name: placeInput.value,
+      link: urlInput.value
+    });
 
-  placeContainer.prepend(newCard)
+    placeContainer.prepend(newCard)
 
-  placeInput.value = '';
-  urlInput.value = '';
-  togglePopup(popupPlace);
+    placeInput.value = '';
+    urlInput.value = '';
+    togglePopup(popupPlace);
   });
 
   addPlace.addEventListener('click', function() {
